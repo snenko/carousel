d23b9beb62dccf0b8eacc364fe05a43e03348877
snenko <snenko@gmail.com>
2015-07-06 18:05:24 +0300

    figure

--- index.php
+++ index.php
@@ -10,43 +10,21 @@
      ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
+    <link href="/css/style.css" type="text/css" rel="stylesheet">
+    <link href="/css/sixangles.css" type="text/css" rel="stylesheet">
+
     <!-- Java Script
      ================================================== -->
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <link href="/css/default_skin_variation.css" type="text/css" rel="stylesheet">
+    <link href="/css/default_skin.css" type="text/css" rel="stylesheet">
 
     <script type="text/javascript" src="/js/jquery.sky.carousel-1.0.2.min.js"></script>
-
-    <script type="text/javascript">
-        $(function() {
-            $('#your-carousel-id').carousel({
-                itemWidth: 170,
-                itemHeight: 240,
-                distance: 15,
-                selectedItemDistance: 50,
-                selectedItemZoomFactor: 1,
-                unselectedItemZoomFactor: 0.67,
-                unselectedItemAlpha: 0.6,
-                motionStartDistance: 170,
-                topMargin: 119,
-                gradientStartPoint: 0.35,
-                gradientOverlayColor: "#f5f5f5",
-                gradientOverlaySize: 190,
-                reflectionDistance: 1,
-                reflectionAlpha: 0.35,
-                reflectionVisible: true,
-                reflectionSize: 70,
-                selectByClick: true
-            });
-        });
-    </script>
-
+    <script type="text/javascript" src="/js/my.js"></script>
 
 <body style="cursor: auto;">
 
 <!-- 960 Container -->
 <div class="container">
-
     <!-- Project Title -->
     <div class="sixteen columns">
         <h4 class="headline">Default Skin Variation1</h4>
@@ -55,31 +33,50 @@
             <div class="sky-carousel-wrapper">
                 <ul class="sky-carousel-container">
                     <li>
-                        <img src="/images/1.jpg" alt="" />
+                        <div class="prew-box">
+                            <img src="/images/1.jpg" alt="" />
+                            <div class="figure-sixangle"><div class="price">$ 150.00</div></div>
+                        </div>
                         <div class="sc-content">
                             <h2>Maria Vann</h2>
                         </div>
                     </li>
+
                     <li>
-                        <img src="/images/2.jpg" alt="" />
+                        <div class="prew-box">
+                            <img src="/images/2.jpg" alt="" />
+                            <div class="figure-sixangle"><div class="price">$ 150.00</div></div>
+                        </div>
                         <div class="sc-content">
                             <h2>Leslie Lawson</h2>
                         </div>
                     </li>
+
                     <li>
-                        <img src="/images/3.jpg" alt="" />
+                        <div class="prew-box">
+                            <img src="/images/3.jpg" alt="" />
+                            <div class="figure-sixangle"><div class="price">$ 150.00</div></div>
+                        </div>
                         <div class="sc-content">
                             <h2>Dora Thomas</h2>
                         </div>
                     </li>
+
                     <li>
-                        <img src="/images/4.jpg" alt="" />
+                        <div class="prew-box">
+                            <img src="/images/4.jpg" alt="" />
+                            <div class="figure-sixangle"><div class="price">$ 150.00</div></div>
+                        </div>
                         <div class="sc-content">
                             <h2>Karen Olsen</h2>
                         </div>
                     </li>
+
                     <li>
-                        <img src="/images/5.jpg" alt="" />
+                        <div class="prew-box">
+                            <img src="/images/5.jpg" alt="" />
+                            <div class="figure-sixangle"><div class="price">$ 150.00</div></div>
+                        </div>
                         <div class="sc-content">
                             <h2>Mittie Phillips</h2>
                         </div>
---
+++ js/my.js
@@ -0,0 +1,32 @@
+$(function() {
+    var carousel = $('#your-carousel-id').carousel({
+        itemWidth: 170,
+        itemHeight: 240,
+        distance: 35,
+        selectedItemDistance: 60,
+        selectedItemZoomFactor: 1,
+        unselectedItemZoomFactor: 0.67,
+        unselectedItemAlpha: 0.6,
+        motionStartDistance: 170,
+        topMargin: 60,
+        gradientStartPoint: 0.35,
+        gradientOverlayColor: "#f5f5f5",
+        gradientOverlaySize: 190,
+//                reflectionDistance: 1,
+//                reflectionAlpha: 0.35,
+//                reflectionVisible: true,
+//                reflectionSize: 70,
+        selectByClick: true
+//        loop: true
+    });
+
+//    carousel.on('itemSelected.sc', function(evt) {
+//    });
+
+});/**
+ * Created with JetBrains PhpStorm.
+ * User: adolis
+ * Date: 06.07.15
+ * Time: 14:29
+ * To change this template use File | Settings | File Templates.
+ */
